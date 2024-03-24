13ed6c08289f5572c55a304d38a5960eaaf0a8e9
Chiyavan Banavi <cbanavi@gmail.com>
2024-03-24 16:27:39 -0700

    Week 1 Tasks

---
+++ src/chiyavan/NoDivisionOperator_Chiyavan.java
@@ -0,0 +1,8 @@
+package chiyavan;
+
+public class NoDivisionOperator_Chiyavan {
+
+    //2. Numbers -- Divide without '/' operator
+    //Write a method that can divide two numbers without using division '/' operator
+
+}
