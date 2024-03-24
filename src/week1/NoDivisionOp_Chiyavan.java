cfc38454a5e761ca6a2f0963deb8bc240d1abda1
Chiyavan Banavi <cbanavi@gmail.com>
2024-03-24 14:43:41 -0700

    Starting week 1 tasks

---
+++ src/week1/NoDivisionOp_Chiyavan.java
@@ -0,0 +1,6 @@
+package week1;
+
+public class NoDivisionOp_Chiyavan {
+    //2. Numbers -- Divide without '/' operator
+    //Write a method that can divide two numbers without using division '/' operator
+}
---
+++ src/week1/NumbersOddEven_Chiyavan.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class NumbersOddEven_Chiyavan {
+
+    //1. Numbers -- odd & even
+    //Write a method which can identify given number is even or odd
+    //Ex: identify(5) -> "Odd"
+    //    identify(6) -> "Even"
+
+}
