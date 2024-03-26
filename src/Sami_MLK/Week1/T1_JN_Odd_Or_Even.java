2737fcf1a4b476372bedcd879a73b248c28b9133
Sami Melakhessou <melakhessou.sami@gmail.com>
2024-03-26 00:17:25 -0700

    Week one for Sami updated

--- src/Sami_MLK/Week1/T1_JN_Odd_Or_Even.java
+++ src/Sami_MLK/Week1/T1_JN_Odd_Or_Even.java
@@ -3,6 +3,23 @@ package Sami_MLK.Week1;
 public class T1_JN_Odd_Or_Even {
 
 //Write  a method which can identifies given number is even or odd
+
+    public static void main(String[] args) {
+        //Write  a method which can identifies given number is even or odd
+        int number = 100;
+        System.out.println(identifyNumberType(number));
+
+    }
+    public static String identifyNumberType(int number) {
+        if (number % 2 == 0) {
+            return "even";
+        } else {
+            return "odd";
+        }
+    }
+
+
+
 }
 
 
