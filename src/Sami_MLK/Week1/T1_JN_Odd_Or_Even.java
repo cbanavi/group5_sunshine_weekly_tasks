f51a18a7f4d183bf9a252dbbb23778b8adef5f12
Sami Melakhessou <melakhessou.sami@gmail.com>
2024-03-26 00:09:34 -0700

    Week one for Sami updated

--- src/Sami_MLK/Week1/T1_JN_Odd_Or_Even.java
+++ src/Sami_MLK/Week1/T1_JN_Odd_Or_Even.java
@@ -1,26 +1,9 @@
 package Sami_MLK.Week1;
 
 public class T1_JN_Odd_Or_Even {
-    public static void main(String[] args) {
-        //Write a method that can divide two numbers without using division operator
 
-        System.out.println(divide(20, 5));
-
-    }
-
-public static double divide(double a, double b) {
-    double result = 0;
-
-    while (a >= b) {
-        a -= b;
-        result++;
-    }
-    return result;
+//Write  a method which can identifies given number is even or odd
 }
-    }
-
-   // This implementation uses a while loop to repeatedly subtract b from a until a is less
-  //than or equal to b. The loop increments result each time this happens.
 
 
 
--- src/Sami_MLK/Week1/T2_JN_Division_Without_Operator.java
+++ src/Sami_MLK/Week1/T2_JN_Division_Without_Operator.java
@@ -2,45 +2,24 @@ package Sami_MLK.Week1;
 
 public class T2_JN_Division_Without_Operator {
     public static void main(String[] args) {
-        specialDivision(7777, 10);
+        //Write a method that can divide two numbers without using division operator
+
+        System.out.println(divide(20, 5));
+
     }
 
-    private static double specialDivision(double x, double y) {
-
-        if (y == 0) {
-            throw new ArithmeticException("Not divisible by zero");
-        }
-
+    public static double divide(double a, double b) {
         double result = 0;
-        double dividend = x;
-        double divisor = y;
-        while (dividend >= divisor) {
-            dividend -= divisor;
+
+        while (a >= b) {
+            a -= b;
             result++;
         }
-
-        System.out.println(x + " divided by " + y + " without using division operator is " + result);
-        System.out.println("This result is based on iterative subtraction, representing division.");
         return result;
     }
 }
 
-/*
+// This implementation uses a while loop to repeatedly subtract b from a until a is less
+//than or equal to b. The loop increments result each time this happens
 
-2. Numbers -- Divide without '/' operator
-Write a method that can divide two numbers without using division '/' operator
 
- */
-/*         EXTRA LINES: in case we will be dealing with negative numbers
-        //we are flipping signs from negative # into positive # so it's suitable for negative #s as well
-        double sign = 1;
-        if (x < 0) {
-            sign = -sign;
-            x = -x;
-        }
-        if (y < 0) {
-            sign = -sign;
-            y = -y;//same logic
-        }
-        return sign * result;
- */
