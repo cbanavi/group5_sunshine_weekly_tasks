2a5f2b98ebda9561ccd8e778fdbedb2fee732353
berna <bernayilmaz0735@gmail.com>
2024-03-26 13:46:03 -0700

    tasks committed

---
+++ src/berna_project/week1/oddOrEven.java
@@ -0,0 +1,8 @@
+package berna_project.week1;
+
+public class oddOrEven {
+    public static void main(String[] args) {
+        //Write  a method which can identifies given number is even or odd
+
+    }
+}
