4e538520de3bfc64c67488de7470ec968b700a81
Konstantin Gavrilov <gavrilovks@gmail.com>
2024-03-26 20:40:26 -0700

    oddOrEven

--- src/konst/week1/oddOrEven_konst.java
+++ src/konst/week1/oddOrEven_konst.java
@@ -1,8 +1,13 @@
+/* Write  a method which can identifies given number is even or odd */
+
 package konst.week1;
 
 public class oddOrEven_konst {
-    public static void main(String[] args) {
-        //Write  a method which can identifies given number is even or odd
+    public static void checkEvenOdd(int number) {
+        System.out.println(number % 2 == 0 ? "even" : "odd");
+    }
 
+    public static void main(String[] args) {
+        checkEvenOdd(12);
     }
 }
