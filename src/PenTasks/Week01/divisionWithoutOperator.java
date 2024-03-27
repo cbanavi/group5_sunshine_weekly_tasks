894188e6488f7dba44e8221f0df3cfdc3b77f27f
pendar <pendarshams@icloud.com>
2024-03-27 01:19:46 -0700

    updated March 26th

--- src/PenTasks/Week01/divisionWithoutOperator.java
+++ src/PenTasks/Week01/divisionWithoutOperator.java
@@ -2,39 +2,45 @@ package PenTasks.Week01;
 
 public class divisionWithoutOperator {
 
-        public static void main(String[] args) {
- divide(15,5);
+    public static void main(String[] args) {
+        divide(-18, 5);
 
+    }
+
+    public static int divide(int dividend, int divisor) {
+// if divisor is equal to zero
+
+        if (divisor == 0) {
+            throw new ArithmeticException("Not divisible by zero");
         }
 
-        public static int divide(int dividend, int divisor) {
-// if divisor is equal to zero
+
+        // for handling negative numbers :
+
+         int coefficient = 1;
+         if ((dividend < 0) ^ (divisor < 0)) {
+       coefficient = -1;}
+
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+
+
+
+
             int quotient = 0;
             int remainder = dividend;
-            if (divisor == 0) {
-                throw new ArithmeticException("Not divisible by zero");
-            }
-
-            while (remainder >= divisor) {
-                remainder -= divisor;
-                quotient++;
-            }
-            // for handling negative numbers :
-           // int coefficient = 1;
-           // if ((divisor < 0 && dividend >= 0) || (divisor >= 0 && dividend < 0)) {
-
-                // if (divisor<0 ^ dividend<0){
-               //coefficient = -1;}
-           // dividend = Math.abs(dividend);
-            // divisor = Math.abs(divisor);
-
-            System.out.println("the remainder is : " + remainder);
-            System.out.println("The quotient is : " + quotient);
-            return quotient;
-            //return quotient * coefficient;
-
+        while (remainder >= divisor) {
+            remainder -= divisor;
+            quotient++;
         }
+
+        System.out.println("the remainder is : " + coefficient*remainder);
+        System.out.println("The quotient is : " + coefficient*quotient);
+        //return quotient;
+        return coefficient*quotient ;
+
     }
+}
 
 
 
