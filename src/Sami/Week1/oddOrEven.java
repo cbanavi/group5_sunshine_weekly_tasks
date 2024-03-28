dddb31c34cf83f38a9b8ae40cf8640b19f55b937
Sami Melakhessou <melakhessou.sami@gmail.com>
2024-03-28 16:05:22 -0700

    Updated

--- src/Sami/Week1/oddOrEven.java
+++ src/Sami/Week1/oddOrEven.java
@@ -1,19 +1,20 @@
 package Sami.Week1;
 
         public class oddOrEven {
-            public static void main(String[] args) {
+            public class NumberTypeIdentifier {
 
+                public static void main(String[] args) {
+                    System.out.println(identifyNumberType(100));
+                    System.out.println(identifyNumberType(37));
+                }
 
-                int number = 100;
-                System.out.println(identifyNumberType(number));
-
-            }
-            public static String identifyNumberType(int number) {
-                if (number % 2 == 0) {
-                    return "even";
-                } else {
-                    return "odd";
+                public static String identifyNumberType(int number) {
+                    if (number % 2 == 0) {
+                        return "even";
+                    } else {
+                        return "odd";
+                    }
                 }
             }
+        }
 
-    }
