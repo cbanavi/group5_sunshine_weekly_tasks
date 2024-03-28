0e64b2201e5e82e30da075082b61f456353800d9
Chiyavan Banavi <cbanavi@gmail.com>
2024-03-27 19:38:24 -0700

    Updated divisionWithoutOperator_Chiyavan to be able to cover each scenario

--- src/chiyavan/week1/divisionWithoutOperator_Chiyavan.java
+++ src/chiyavan/week1/divisionWithoutOperator_Chiyavan.java
@@ -13,15 +13,31 @@ public class divisionWithoutOperator_Chiyavan {
 
         double quotient = 0;
 
-        if (divisor >= 0) {
+        if (divisor >= 0) {                      //This runs when divisor is positive and dividend is positive //works
             while (dividend >= divisor) {
                 dividend -= divisor;
                 quotient++;
             }
         }
 
-        if (divisor < 0) {
+        if (divisor < 0 && dividend < 0) {      //This runs when divisor is negative and dividend is negative //works
+            while (-dividend >= -divisor) {
+                dividend -= divisor;
+                quotient++;
+            }
+
+        }
+
+        if (divisor < 0) {                      //This runs when divisor is negative and dividend is positive //works
             while (dividend >= -divisor) {
+                dividend += divisor;
+                quotient--;
+            }
+
+        }
+
+        if (divisor > 0 && dividend < 0) {      //This runs when divisor is positive and dividend is negative //works
+            while (-dividend >= divisor) {
                 dividend += divisor;
                 quotient--;
             }
@@ -34,7 +50,7 @@ public class divisionWithoutOperator_Chiyavan {
     }
 
     public static void main(String[] args) {
-        divisionWithoutOperator(15, -3);
+        divisionWithoutOperator(-15, 3);
     }
 
 }
