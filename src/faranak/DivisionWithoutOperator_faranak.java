9585f83a90187e9c354602bdfba6d663fad8cb9a
faranak <faranak.jesus@yahoo.com>
2024-03-28 21:20:12 -0700

    commit changes

--- src/faranak/DivisionWithoutOperator_faranak.java
+++ src/faranak/DivisionWithoutOperator_faranak.java
@@ -4,31 +4,22 @@ public class DivisionWithoutOperator_faranak {
 
     public static void main(String[] args) {
 
-        int result = dividingWithOutOperator(100, -5);
-        System.out.println(result);
+        division(10, 2);
     }
-    public static int dividingWithOutOperator(int n1, int n2){
-        if (n1 == 0 || n2 == 0){
-            throw new ArithmeticException("Number can not be divided by zero");
-        }
-        // for negative numbers
-        if (n2 < 0){
-            return -dividingWithOutOperator(n1, -n2);
-        }
-        int quotient = 0;
 
-        while (n1 >= n2){
-            n1 = n1 - n2;
-            quotient++;
+    public static void division(int a, int b) {
 
+        if (b == 0) {
+            throw new ArithmeticException("cannot divide by zero");
         }
-        return quotient;
+        System.out.println(Math.floorDiv(a, b));
+
 
     }
 
+}
 
 
-    }
 
 
 // Create a method that divide two numbers without operator
