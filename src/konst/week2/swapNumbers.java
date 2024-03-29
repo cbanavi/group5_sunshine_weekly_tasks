04b24d067d0cb4e1aebeef89d7c12404deb58d15
Konstantin Gavrilov <gavrilovks@gmail.com>
2024-03-29 11:24:59 -0700

    swapNumbers

--- src/konst/week2/swapNumbers.java
+++ src/konst/week2/swapNumbers.java
@@ -4,9 +4,18 @@ Swap two variable' values without using a third variable
 */
 package konst.week2;
 
+
 public class swapNumbers {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
 
+        System.out.println("Before swap: x = " + x + ", y = " + y);
+
+        x = x + y;
+        y = x - y;
+        x = x - y;
+
+        System.out.println("After swap: x = " + x + ", y = " + y);
+    }
 }
-
-
-
