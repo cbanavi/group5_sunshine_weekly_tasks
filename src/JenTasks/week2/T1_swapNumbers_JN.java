9657c7b2e88a836608a093086e016ec2a1910dab
Jen Nest <154011445+jenneft@users.noreply.github.com>
2024-03-29 09:28:06 -0700

    Week 2: adjusted the method's formula from multiplication and division to addition and subtraction.

--- src/JenTasks/week2/T1_swapNumbers_JN.java
+++ src/JenTasks/week2/T1_swapNumbers_JN.java
@@ -2,16 +2,17 @@ package JenTasks.week2;
 
 public class T1_swapNumbers_JN {
     public static void main(String[] args) {
-        int x = 777;
-        int y = 11;
+        int x = 77;
+        int y = 7;
         swapNumbers(x, y);
     }
 
     public static void swapNumbers(int x, int y) {
+
         System.out.println("Before swap: " + "x= " + x + " and " + "y= " + y);
-        x = x * y;
-        y = x / y;
-        x = x / y;
+        x = x + y;
+        y = x - y;
+        x = x - y;
 
         System.out.println("After swap: " + " x= " + x + " and " + "y= " + y);
 
