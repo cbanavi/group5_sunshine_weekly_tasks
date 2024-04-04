802199ab4e73da5fbe9c539bea4d0aeae40a3a25
faranak <faranak.jesus@yahoo.com>
2024-04-04 01:21:55 -0700

    week2 tasks

---
+++ src/faranak/week2/ConsecutiveNumbers.java
@@ -0,0 +1,32 @@
+package faranak.week2;
+
+public class ConsecutiveNumbers {
+
+    public static void main(String[] args) {
+
+        int N = 100;
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
+                System.out.println("CodilityTestCoders");
+            } else if (i % 2 == 0) {
+                System.out.println("Codility");
+            } else if (i % 3 == 0) {
+                System.out.println("Test");
+            } else if (i % 5 == 0){
+                System.out.println("Coders");
+            }else{
+                System.out.println(i);
+            }
+
+        }
+        }
+    }
+
+// Numbers —— print consecutive numbers
+// Write a function
+// that, given a positive integer N, prints the consecutive numbers from 1 to N, each on a separate line.
+// However, any number divisible by 2, 3 or 5 should be replaced by the word Codility, Test or Coders respectively.
+// If a number is divisible by more than one of the numbers: 2, 3 or 5, it should be
+//replaced by a concatenation of the respective words Codility, Test and Coders in this given order.
+//For example: numbers divisible by both 2 and 3 should be replaced by CodilityTest
+//and numbers divisible by all three numbers: 2, 3 and 5, should be replaced by CodilityTestCoders.
---
+++ src/faranak/week2/SwapNumbers.java
@@ -0,0 +1,28 @@
+package faranak.week2;
+
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+
+        int x = 12;
+        int y = 18;
+        System.out.println("Before swap: x = " + x + ", y = " + y);
+
+        int temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("After swap: x = " + x + ", y = " + y);
+
+    }
+}
+
+
+//Numbers —— Swap Numbers
+//Swap two variable's values without using a third variable
+
+
+
+
+
+
