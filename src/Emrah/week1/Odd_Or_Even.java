92eff718fb4b8754abf687fd33470835a60b5248
emrahltr <emrahltr@gmail.com>
2024-04-05 02:17:54 -0700

    some changes for week1

--- src/Emrah/week1/Odd_Or_Even.java
+++ src/Emrah/week1/Odd_Or_Even.java
@@ -3,7 +3,9 @@ package Emrah.week1;
 public class Odd_Or_Even {
     public static void main(String[] args) {
         int n = 2;
-        if(n%2==0){
+        // The modulo operator returns the remainder of the division of its first operand by its second operand.
+        // In this case, n is divided by 2 and the remainder is 0, so the condition is always true.
+        if (n % 2 == 0) {
             System.out.println("The "+n+" is an even number");
         }else {
             System.out.println("The "+n+" is an odd number");
