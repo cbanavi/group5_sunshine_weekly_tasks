451cd57c3abb476a53b4cbd3d89ee8ff438ab96a
cbanavi <cbanavi@gmail.com>
2024-04-08 14:23:39 -0700

    Week 3 and Week 4 tasks have been inputted

---
+++ src/week3_template/ArmstrongNumbers.java
@@ -0,0 +1,13 @@
+package week3_template;
+
+public class ArmstrongNumbers {
+}
+
+/*
+Write a method that can check if a number is
+Armstrong number
+
+Armstrong number - An Armstrong number of three digits is an integer
+such that the sum of the cubes of its digits is equal to the number itself.
+For example, 371 is an Armstrong number since 3*3*3 + 7*7*7 + 1*1*1 = 371.
+ */
