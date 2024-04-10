b47f0d1e3b64c63274dfa24414011b11fea701f6
Konstantin Gavrilov <gavrilovks@gmail.com>
2024-04-10 13:37:46 -0700

    SameLetters

--- src/konst/week4/SameLetters.java
+++ src/konst/week4/SameLetters.java
@@ -5,6 +5,27 @@ same("abc", "abb"); -> false:
 */
 package konst.week4;
 
+import static konst.week4.FrequencyOfCharacters.frequencyOfChars;
+
 public class SameLetters {
+
+    public static void main(String[] args) {
+        System.out.println(isSameLetters("abc", "cab"));
+        System.out.println(isSameLetters("abc", "abb"));
+    }
+
+    public static boolean isSameLetters(String str1, String str2) {
+        // Handle strings of different lengths
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        // Calculate character frequencies
+        String freq1 = frequencyOfChars(str1);
+        String freq2 = frequencyOfChars(str2);
+
+        // Compare frequency strings
+        return freq1.equals(freq2);
+    }
 }
 
