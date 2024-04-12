27e9758d3414d769739d443aa22b44a3784973e4
faranak <faranak.jesus@yahoo.com>
2024-04-12 12:16:42 -0700

    week 4 tasks

---
+++ src/faranak/week04/FrequencyOfCharacters.java
@@ -0,0 +1,20 @@
+package faranak.week04;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public record FrequencyOfCharacters() {
+
+    public static void main(String[] args) {
+        String input = "AAABBCDD";
+        Map<Character,Integer> frequencies = new HashMap<>();
+        for (char ch : input.toCharArray())
+            frequencies.merge(ch, 1, Integer::sum);
+        System.out.println(frequencies);
+        }
+             }
+/*
+Write a return method that can find the frequency of
+characters
+Ex: FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+ */
---
+++ src/faranak/week04/RemoveDuplicates.java
@@ -0,0 +1,33 @@
+package faranak.week04;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+       String input = new String("AAABBBCCC");
+       System.out.println(removeDup(input));
+
+    }
+    public static String removeDup(String input) {
+
+        //Splits the input string into characters,removes duplicates and joins them back into a single string
+        String collect = new HashSet<>(Arrays.stream(input.split("")).collect(Collectors.toList())).stream().collect(Collectors.joining());
+        if(input.isEmpty()){
+            return input;
+        }
+        else{
+            return collect;
+
+        }
+    }
+    }
+/*
+Write a return method that can remove the duplicated values from
+String
+Ex: removeDup("AAABBBCCC") ==> ABC
+
+ */
