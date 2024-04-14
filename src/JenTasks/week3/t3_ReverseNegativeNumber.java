7babfa82157c898ed27d4096f0abdc8f6036a4e3
Jen Nest <154011445+jenneft@users.noreply.github.com>
2024-04-14 15:01:52 -0700

    Week 3: revised to reflect correct logic for the given task

--- src/JenTasks/week3/t3_ReverseNegativeNumber.java
+++ src/JenTasks/week3/t3_ReverseNegativeNumber.java
@@ -6,14 +6,20 @@ public class t3_ReverseNegativeNumber {
             throw new IllegalArgumentException("Function for negative numbers only.");
         }
         int result = 0;
-        if (n < 0) {
-            result = n * (-1);
+
+        while (n != 0) {
+            int remainder = n % 10; // retrieve the last digit ex: -112%10=-11.2; -2 is a remainder
+            //-11%10=-1.1; -1 is a remainder; -1%10= -0.1; -1 is the last remainder
+            result = result * 10 + remainder;//append to the result ex: 0*10+(-2)=-2
+            //-2*10+(-2)=-20+(-1)=-21 // -21*10(-1)=-210+(-1)=-211;
+            n /= 10; // -112/10=11; //-11/10=-1 //-1/10=0
+
         }
         return result;
     }
 
     public static void main(String[] args) {
-        int[] negatives = {-1, -259, -7821, -8912};
+        int[] negatives = {-18,-112, -259, -7821, -8912};
 
         for (int each : negatives) {
             System.out.println(reverseNegative(each));
