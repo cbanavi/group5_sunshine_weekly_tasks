60d39e3ddef479b0de68a06f5eac4646e6c40cc3
pendar <pendarshams@icloud.com>
2024-04-15 16:53:48 -0700

    corrected April 24th

--- src/PenTasks/Week03/PrimeNumber.java
+++ src/PenTasks/Week03/PrimeNumber.java
@@ -10,13 +10,17 @@ public class PrimeNumber {
         if (num <= 1) {
             System.out.println("Please choose a number greater than 1!");
             return false;
-        } else if (num % num == 0) {
-            System.out.print(num + " is a prime number.");
-            return true;
-        } else {
-            System.out.print(num + " is not a prime number.");
-            return false;
         }
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                System.out.print(num + " is not a prime number.");
+                return false;
+            }
+
+
+        }
+        System.out.print(num + " is a prime number.");
+        return true;
 
     }
 }
