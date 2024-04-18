2ff6a29c385de1551521b7d72ebc4ce12851c0f4
pendar <pendarshams@icloud.com>
2024-04-17 19:29:56 -0700

    corrected April 16th

--- src/PenTasks/Week04/RemoveDuplicates.java
+++ src/PenTasks/Week04/RemoveDuplicates.java
@@ -9,26 +9,6 @@ public class RemoveDuplicates {
         System.out.println(removeDup2("AAADDDFFC"));
 
     }
-/* My attempt to use removeIf but it didnt work
-    public static String removeDup1(String str) {
-        // Convert the input string to a list of characters
-        List<Character> charList = new ArrayList<>();
-        for (char each : str.toCharArray()) {
-            charList.add(each);
-        }
-
-        // using removeIf for dups
-        charList.removeIf(ch -> charList.indexOf(ch) != charList.lastIndexOf(ch));
-
-        // making a string
-        StringBuilder result = new StringBuilder(charList.size());
-        for (Character ch : charList) {
-            result.append(ch);
-        }
-
-        // StringBuilder to a string
-        return result.toString();
-    } */
 
 
 public static String removeDup2 (String str){
@@ -56,3 +36,23 @@ String
 Ex: removeDup("AAABBBCCC") ==> ABC
 
  */
+/* My attempt to use removeIf but it didnt work
+    public static String removeDup1(String str) {
+        // Convert the input string to a list of characters
+        List<Character> charList = new ArrayList<>();
+        for (char each : str.toCharArray()) {
+            charList.add(each);
+        }
+
+        // using removeIf for dups
+        charList.removeIf(ch -> charList.indexOf(ch) != charList.lastIndexOf(ch));
+
+        // making a string
+        StringBuilder result = new StringBuilder(charList.size());
+        for (Character ch : charList) {
+            result.append(ch);
+        }
+
+        // StringBuilder to a string
+        return result.toString();
+    } */
