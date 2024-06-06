e94e41450a06fd597f95257f53fdf03916dbdff3
bernasantiago <bernayilmaz0735@gmail.com>
2024-06-06 10:48:53 -0700

    week5-6 completed

--- src/week6_template/SumOfDigitsInString.java
+++ src/week6_template/SumOfDigitsInString.java
@@ -1,9 +1,4 @@
 package week6_template;
 
-public class SumOfDigitsInString {
-}
+public class SumOfDigitsInString {}
 
-/*
-String -- sum of digits in a string
-Write a method that can return the sum of the digits in a string
- */
