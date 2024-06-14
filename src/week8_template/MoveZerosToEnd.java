d2e3f9aca15351d027a461412d7597425383a402
cbanavi <109002559+cbanavi@users.noreply.github.com>
2024-06-13 21:34:46 -0700

    Added week 8 tasks

--- src/week8_template/MoveZerosToEnd.java
+++ src/week8_template/MoveZerosToEnd.java
@@ -4,8 +4,10 @@ public class MoveZerosToEnd {
 }
 
 /*
+
 Array - Move Zeros to the End
 Write a method that can move all the zeros to last indexes of the array (Do
 Not Use Sort Method) Ex: input: {1,0,2,0,3,0,4,0}; output: [1, 2, 3, 4, 0, 0, 0,
 0]
+
  */
