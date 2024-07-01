7e1755b8f02bea0c0d0c7d44fa5b8fa1b44afaed
cbanavi <109002559+cbanavi@users.noreply.github.com>
2024-07-01 16:17:52 -0700

    Added week 9 tasks

---
+++ src/week9_template/RemoveArrayInts.java
@@ -0,0 +1,9 @@
+package week9_template;
+
+public class RemoveArrayInts {
+}
+
+/*
+3) ArrayList - Remove some values
+Given a list of Integers 1, 2, 3, 4, 5, 6 ....etc. remove all values greater than 100.
+ */
---
+++ src/week9_template/RemoveArrayNames.java
@@ -0,0 +1,10 @@
+package week9_template;
+
+public class RemoveArrayNames {
+}
+
+/*
+2) ArrayList - Remove "Ahmed"
+Given a list of people' names: "Ahmed", "John", Eric", "Ahmed"..... Write a java operation to remove all the names named Ahmed
+
+ */
